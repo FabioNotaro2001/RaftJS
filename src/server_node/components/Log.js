40b97a44226d26cc8c0111767b423e391ea9898d
LeoGiaco <leogiaco2@gmail.com>
2023-12-07 22:18:15 +0100

    Revert "Commit a few changes"
    This reverts commit 809eef317dfc57537809516dbb0513b1b609cf67.

--- src/server_node/components/Log.js
+++ src/server_node/components/Log.js
@@ -40,15 +40,15 @@ export class UserCreateData {
 export class AuctionCreateData {
     /**
      * Creates auction creation data.
-     * @param {String} username - The user creating the auction.
+     * @param {String} user - The user creating the auction.
      * @param {Date} startDate - The start date of the auction.
      * @param {String} objName - The name of the auctioned object.
      * @param {String} objDesc - The description of the auctioned object.
      * @param {Number} startPrice - The starting price of the auction.
      */
-    constructor(username, startDate, objName, objDesc, startPrice) {
+    constructor(user, startDate, objName, objDesc, startPrice) {
         /** @type {String} */
-        this.username = username;
+        this.user = user;
         /** @type {Date} */
         this.startDate = startDate;
         /** @type {String} */
@@ -83,13 +83,13 @@ export class AuctionCloseData {
 export class BidCreateData {
     /**
      * Creates bid creation data.
-     * @param {String} username - The user making the bid.
+     * @param {String} user - The user making the bid.
      * @param {Number} auctionId - The ID of the auction.
      * @param {Number} value - The value of the bid.
      */
-    constructor(username, auctionId, value) {
+    constructor(user, auctionId, value) {
         /** @type {String} */
-        this.username = username;
+        this.user = user;
         /** @type {Number} */
         this.auctionId = auctionId;
         /** @type {Number} */
